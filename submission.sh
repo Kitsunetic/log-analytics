35378c8aa85d75ec9bc01a6ef3815c41635b7f03
Kitsunetic <kitsune.guru@gmail.com>
2021-05-07 14:46:37 +0900

    update submission

--- submission.sh
+++ submission.sh
@@ -1,4 +1,4 @@
-python main_submission.py config/distilbert-base-uncased.yaml 2 1
-python main_submission.py config/distilbert-base-uncased.yaml 2 2
-python main_submission.py config/distilbert-base-uncased.yaml 2 3
-python main_submission.py config/distilbert-base-uncased.yaml 2 4
+python main_submission.py config/distilbert-base-uncased-ver3.yaml 1 1
+python main_submission.py config/distilbert-base-uncased-ver3.yaml 1 2
+python main_submission.py config/distilbert-base-uncased-ver3.yaml 1 3
+python main_submission.py config/distilbert-base-uncased-ver3.yaml 1 4
