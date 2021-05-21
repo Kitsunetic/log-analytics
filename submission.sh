fc71dc085f2f69123b5a78c9beb61997dc64945e
Kitsunetic <1996.jh.shim@gmail.com>
2021-05-21 19:49:58 +0900

    remove unused files

--- submission.sh
+++
@@ -1,4 +0,0 @@
-python main_submission.py config/distilbert-base-uncased-ver3.yaml 1 1
-python main_submission.py config/distilbert-base-uncased-ver3.yaml 1 2
-python main_submission.py config/distilbert-base-uncased-ver3.yaml 1 3
-python main_submission.py config/distilbert-base-uncased-ver3.yaml 1 4
